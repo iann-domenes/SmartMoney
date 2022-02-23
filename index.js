479fa59bcb6196b9dc7a675f34fae56d3eb7c4e2
iann-domenes <74912076+iann-domenes@users.noreply.github.com>
2022-02-22 22:58:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,93 @@
-/**
- * @format
- */
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  Modal,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+import {getAllCategories} from '../../../services/Categories';
 
-AppRegistry.registerComponent(appName, () => App);
+import Colors from '../../../styles/Colors';
+
+const NewEntryCategoryPicker = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [allCategories, setAllcategories] = useState([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const data = await getAllCategories();
+      setAllcategories(data);
+    }
+
+    loadCategories();
+
+    console.log('NewEntryCategoryPicker :: useEffect');
+  }, []);
+
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.pickerButton}
+        onPress={() => {
+          setModalVisible(true);
+        }}>
+        <Text style={styles.pickerButtonText}>Alimentação</Text>
+      </TouchableOpacity>
+      <Modal animationType="slide" transparent={false} visible={modalVisible}>
+        <View style={styles.modal}>
+          <FlatList
+            data={allCategories}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity style={styles.modalItem}>
+                <Text style={styles.modalItemText}>{item.name}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() => {
+            setModalVisible(false);
+          }}>
+          <Text>Fechar</Text>
+        </TouchableOpacity>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  pickerButton: {
+    backgroundColor: Colors.asphalt,
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    padding: 20,
+  },
+  pickerButtonText: {
+    fontSize: 28,
+    color: Colors.white,
+    textAlign: 'center',
+  },
+  modalItem: {
+    backgroundColor: Colors.asphalt,
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    padding: 20,
+  },
+  modalItemText:{
+    fontSize: 22,
+    color: Colors.white,
+    textAlign: 'center',
+  },
+});
+
+export default NewEntryCategoryPicker;
